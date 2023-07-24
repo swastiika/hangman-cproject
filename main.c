7a79a4787555c3bc6f756c50e6239386eab090fd
Prazwal Chapagain <65542395+PRAZPC@users.noreply.github.com>
2023-07-24 13:11:17 +0000

    added insight of code

---
+++ main.c
@@ -0,0 +1,52 @@
+#include<stdio.h>
+#include<stdlib.h>
+#include<string.h>
+
+int main ()
+{
+// this is only the outlook how the game should word with a fixed word not the final code
+printf("\t HANG-MAN \n ");
+
+int i ,j,k;
+int mistake=0 , errors=0, try=3, ans=0;
+
+char word[]="abc";
+char emp='_';
+char inp;
+char b=word[1];
+
+printf("%c %c %c \n",word[0],emp,word[2]); // can be used loop for printing in final program
+printf("enter the missing letter:\n");
+
+while(try!=0){
+    scanf(" %c",&inp);
+if(inp==b){
+                    ans++;
+                    printf("you are safe form hanging\n");
+                    return 0;
+
+
+}
+else{
+    printf("you have only %d life left \n",try);
+    try--;
+}
+if(try==0){
+                printf("\n the game is over: you are hanged \n");
+
+}
+
+}
+
+
+
+
+
+
+
+
+
+
+
+
+}
